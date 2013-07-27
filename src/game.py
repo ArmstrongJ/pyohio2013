032a02cb47fe933e423a06c4f79eeddecb5b8e68
Jeff Armstrong <jeff@approximatrix.com>
2013-07-27 14:27:20 -0400

    everywhere

--- src/game.py
+++ src/game.py
@@ -1,8 +1,58 @@
+import cmd
+from room import get_room
+import textwrap
 
-class Game():
+class Game(cmd.Cmd):
     def __init__(self):
-        pass
+        cmd.Cmd.__init__(self)
+        
+        self.loc = get_room(1)
+        self.look()
+        
+    def move(self, dir):
+        newroom = self.loc._neighbor(dir)
+        if newroom is None:
+            print("you can't go that way")
+        else:
+            self.loc = get_room(newroom)
+            self.look()
+        
+    def look(self):
+        print(self.loc.name)
+        print("")
+        for line in textwrap.wrap(self.loc.description, 72):
+            print(line)
+    
+    def do_up(self, args):
+        """Go up"""
+        self.move('up')
+    
+    def do_down(self, args):
+        """Go down"""
+        self.move('down')
+    
+    
+    def do_n(self, args):
+        """Go north"""
+        self.move('n')
+    
+    def do_s(self, args):
+        """Go south"""
+        self.move('s')
+        
+    def do_e(self, args):
+        """Go east"""
+        self.move('e')
+    
+    def do_w(self, args):
+        """Go west"""
+        self.move('w')
+    
+    def do_quit(self, args):
+        """Leaves the game"""
+        print("Thank you for playing")
+        return True
         
 if __name__ == "__main__":
     g = Game()
-    
+    g.cmdloop()
--- src/room.py
+++ src/room.py
@@ -1,3 +1,14 @@
+import json
+
+
+def get_room(id):
+    ret = None
+    with open(str(id)+".json", "r") as f:
+        jsontext = f.read()
+        d = json.loads(jsontext)
+        d['id'] = id
+        ret = Room(**d)
+    return ret
 
 class Room():
     def __init__(self, id=0, name="A Room", description="An empty room", neighbors={}):
